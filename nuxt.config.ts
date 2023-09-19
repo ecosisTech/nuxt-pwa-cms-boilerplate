f6e2a057927ca8c4c1e629aee4490c9c8edae339
equaliser0 <accounts@equaliser.minimalcity.de>
2023-09-19 11:04:07 +0200

    change project dir

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  srcDir: './src',
+  srcDir: './app',
   modules: [
     '@vite-pwa/nuxt',
     '@nuxtjs/tailwindcss',
