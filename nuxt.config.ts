961741d01ff34b81cbc00a291e82b9dec399fbfe
equaliser0 <accounts@equaliser.minimalcity.de>
2023-09-19 11:24:18 +0200

    add lucide icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,7 @@ export default defineNuxtConfig({
     '@nuxtjs/tailwindcss',
     '@nuxtjs/color-mode',
     '@pinia/nuxt',
+    'nuxt-lucide-icons'
   ],
   autoImports: [
     ['defineStore', 'definePiniaStore'],
