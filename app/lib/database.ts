bbe12737203cec87c0a11d916a00ef42c0d212aa
equaliser0 <accounts@equaliser.minimalcity.de>
2023-09-20 10:41:08 +0200

    add user database

--- app/lib/database.ts
+++ app/lib/database.ts
@@ -1,7 +1,12 @@
 import Level from 'level-ts'
 
 const database = new Level.default('./.db') // TODO Fix
+const userDatabase = new Level.default('./.db/user') // TODO Fix
 
 export const useDatabase = () => {
   return database
+}
+
+export const useUserDatabase = () => {
+  return userDatabase
 }
