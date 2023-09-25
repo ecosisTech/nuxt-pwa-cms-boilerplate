d6dfd231bb13b88921fd6ce46b1144d8b091543d
equaliser0 <accounts@equaliser.minimalcity.de>
2023-09-25 10:09:15 +0200

    update

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,4 +1,12 @@
 <script setup lang="ts">
+import { useThemeStore } from '../stores/theme'
+import { useCartStore } from '../stores/cart'
+
+const router = useRouter()
+
+const themeStore = useThemeStore()
+const cartStore = useCartStore()
+
 const cookie = useCookie('cookie')
 
 const acceptCookies = () => {
@@ -20,47 +28,91 @@ const acceptCookies = () => {
         </div>
       </div>
     </div>
-    <div class="navbar bg-base-100 fixed">
-      <div class="flex-1">
-        <a class="btn btn-ghost normal-case text-xl"><img class="w-12 mr-4" src="/favicon.ico"></a>
+    <div class="navbar bg-base-100 fixed shadow">
+      <div class="flex-1 hidden md:block">
+        <a class="btn btn-ghost normal-case text-xl" @click="router.push('/')"><img class="w-12 mr-4" src="/favicon.ico"></a>
       </div>
-      <div class="flex-none">
-        <div class="dropdown dropdown-end">
-          <label tabindex="0" class="btn btn-ghost btn-circle">
-            <div class="indicator">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-              <span class="badge badge-sm indicator-item">8</span>
-            </div>
-          </label>
-          <div tabindex="0" class="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
-            <div class="card-body">
-              <span class="font-bold text-lg">8 Items</span>
-              <span class="text-info">Subtotal: $999</span>
-              <div class="card-actions">
-                <button class="btn btn-primary btn-block">View cart</button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="dropdown dropdown-end">
-          <label tabindex="0" class="btn btn-ghost btn-circle avatar">
-            <div class="w-10 rounded-full">
-              <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-            </div>
+      <div class="navbar-start">
+        <div class="dropdown">
+          <label tabindex="0" class="btn btn-ghost lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
           </label>
           <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-            <li>
-              <a class="justify-between">
-                Profile
-                <span class="badge">New</span>
-              </a>
+            <li tabindex="0">
+              <details>
+                <summary>Shop</summary>
+                <ul class="p-2">
+                  <li><a>Home</a></li>
+                  <li><a>Products</a></li>
+                  <li><a>Cataloques</a></li>
+                </ul>
+              </details>
             </li>
-            <li><a>Settings</a></li>
-            <li><a>Logout</a></li>
+            <li><a>Blog</a></li>
           </ul>
         </div>
       </div>
+      <div class="navbar-center hidden lg:flex">
+        <ul class="menu menu-horizontal px-1">
+          <li tabindex="0">
+            <details>
+              <summary>Shop</summary>
+              <ul class="p-2">
+                <li><a @click="router.push('/shop/')">Home</a></li>
+                <li><a>Products</a></li>
+                <li><a>Cataloques</a></li>
+              </ul>
+            </details>
+          </li>
+          <li><a>Blog</a></li>
+        </ul>
+      </div>
+      <div class="navbar-end">
+        <div class="">
+          <input type="checkbox" class="toggle" @click="themeStore.toggleMode()" />
+        </div>
+        <div class="flex-none">
+          <div class="dropdown dropdown-end">
+            <label tabindex="0" class="btn btn-ghost btn-circle">
+              <div class="indicator">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+                <span class="badge badge-sm indicator-item">{{ cartStore.items.length }}</span>
+              </div>
+            </label>
+            <div tabindex="0" class="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
+              <div class="card-body">
+                <span class="font-bold text-lg">8 Items</span>
+                <span class="text-info">Subtotal: 999€</span>
+                <div class="card-actions">
+                  <button class="btn btn-primary btn-block" @click="router.push('/shop/cart')">View cart</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="dropdown dropdown-end">
+            <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+              <div class="w-10 rounded-full">
+                <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+              </div>
+            </label>
+            <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+              <!-- <li>
+                <a class="justify-between">
+                  Profile
+                  <span class="badge">New</span>
+                </a>
+              </li> -->
+              <li><a @click="router.push('/admin/settings')">Settings</a></li>
+              <li><a>Logout</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
-    <slot/>
+    <section>
+      <div class="bg-base-200 pt-16 min-h-screen">
+        <slot/>
+      </div>
+    </section>
   </div>
 </template>
