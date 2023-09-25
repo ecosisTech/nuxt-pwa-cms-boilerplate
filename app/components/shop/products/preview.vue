c1b73a99882b35513e9e09d624603470d862787d
equaliser0 <accounts@equaliser.minimalcity.de>
2023-09-25 10:09:03 +0200

    add shop components

---
+++ app/components/shop/products/preview.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+const props = defineProps({
+  product: {
+    type: Object,
+    required: true
+  }
+})
+
+const image = useAssets(`/assets/images/products/${props.product.image}`)
+
+</script>
+<template>
+  <div>
+    <div  class="max-w-sm bg-white rounded-lg border border-indigo-200 shadow-md dark:bg-gray-800 ">
+      <nuxt-link :to="`product/${product.slug}`">
+      <div>
+        <img class="rounded-t-lg h-80 w-96 object-cover hover:animate-pulse"
+        :src="image"
+        :alt="`${product.name}'s image`"
+        >
+      </div>
+      <div class="p-5">
+        <div>
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-indigo-600 dark:text-white">
+            {{ product.name }}
+          </h5>
+        </div>
+        <p class="mb-3 font-medium text-indigo-700 dark:text-gray-400">
+          {{product.price}}€
+        </p>
+      </div>
+    </nuxt-link>
+  </div>
+</div>
+</template>
