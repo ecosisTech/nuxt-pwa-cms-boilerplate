3d4e5fd94cf1c045bf94cc73b5514ee9f1eac1d9
equaliser0 <chris@elabs.enterprises>
2023-09-26 18:56:04 +0200

    add additional cart functionality

--- app/pages/shop/cart.vue
+++ app/pages/shop/cart.vue
@@ -1,13 +1,19 @@
-<script setup lang="">
+<script setup lang="ts">
 import { useCartStore } from '../../stores/cart'
 
 const cartStore = useCartStore()
+
+const router = useRouter()
 </script>
 
  <template>
-   <div class="content mx-auto artboard artboard-horizontal phone-5 bg-base-100 m-8">
-     <div class="overflow-x-auto">
-       <table class="table rounded">
+   <div class="container mx-auto m-8">
+     <div class="w-full flex flex-col justify-center items-center" v-if="!cartStore.items.length">
+       You have no items in your cart!
+       <NuxtLink class="btn btn-primary mt-4" to="/shop">Go shopping</NuxtLink>
+     </div>
+     <div class="overflow-x-auto bg-base-100 rounded" v-else>
+       <table class="table">
          <!-- head -->
          <thead>
            <tr>
@@ -24,7 +30,8 @@ const cartStore = useCartStore()
          </thead>
          <tbody>
            <!-- row 1 -->
-           <tr v-for="item in cartStore.items">
+           <tr class="hover:bg-base-300" v-for="item in cartStore.items">
+           <!-- <tr class="hover:bg-base-300" v-for="item in cartStore.items" @click="router.push(`shop/product/${item.slug}`)"> -->
              <th>
                <label>
                  <input type="checkbox" class="checkbox" />
@@ -34,32 +41,53 @@ const cartStore = useCartStore()
                <div class="flex items-center space-x-3">
                  <div class="avatar">
                    <div class="mask mask-squircle w-12 h-12">
-                     <img :src="useAssets(`/public/uploads/shop/products/${item.image}`)" alt="Avatar Tailwind CSS Component" />
+                     <img :src="`/uploads/shop/products/${item.image}`" />
                    </div>
                  </div>
+                   <div class="flex items-center space-x-3">
+                     <div>
+                       <div class="font-bold">{{ item.name }}</div>
+                     </div>
+                   </div>
+               </div>
+             </td>
+             <td>
+               <div class="flex items-center space-x-3">
                  <div>
-                   <div class="font-bold">{{ item.name }}</div>
                    <div class="text-sm opacity-50">{{ item.price }}</div>
                  </div>
                </div>
              </td>
+             <td>
+               <div class="flex items-center space-x-3">
+                 <div>
+                   <input type="number" class="text-sm input input-bordered w-24" v-model="item.quantity"/>
+                 </div>
+               </div>
+             </td>
              <th>
-               <button class="btn btn-ghost btn-xs">details</button>
+               <button class="btn btn-ghost btn-xs" @click="cartStore.removeFromCart(item)">Remove</button>
              </th>
            </tr>
          </tbody>
          <!-- foot -->
          <tfoot>
            <tr>
-           <th></th>
-           <th>Product</th>
-           <th>Price</th>
-           <th>Many</th>
-           <th></th>
+             <th></th>
+             <th>Product</th>
+             <th>Price</th>
+             <th>Many</th>
+             <th></th>
            </tr>
          </tfoot>
 
        </table>
+     </div>
+     <div class="w-full flex flex-wrap justify-center">
+       <NuxtLink class="btn btn-primary m-2" to="/shop/checkout">Checkout</NuxtLink>
+       <div class="m-2">
+         <h2>Total: <b>{{ cartStore.calcTotal() }}</b></h2>
+       </div>
      </div>
    </div>
  </template>
