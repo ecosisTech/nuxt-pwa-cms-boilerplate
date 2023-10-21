2eef27f4143b8d1971f9dd81cf3bc485164cae7e
equaliser0 <accounts@equaliser.minimalcity.de>
2023-10-21 19:43:12 +0200

    add Product Adder

---
+++ app/pages/admin/shop/new.vue
@@ -0,0 +1,17 @@
+<script setup lang="">
+import { useProductsStore } from '../../../stores/products'
+
+const productsStore = useProductsStore()
+
+const route = useRoute()
+
+const product = computed(() => {
+  return productsStore.products.find(p => p['product-id'].toString() === route.params.id)
+})
+</script>
+
+ <template>
+   <div class="pt-24">
+     <ShopProductsEditor :product="undefined"/>
+   </div>
+ </template>
