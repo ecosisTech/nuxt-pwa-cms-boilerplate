817c20625159512496253ecf4f677dc06b2a15e7
equaliser0 <accounts@equaliser.minimalcity.de>
2023-10-23 19:34:08 +0200

    edit for ID resolver

--- app/lib/interfaces/subcategories.interface.ts
+++ app/lib/interfaces/subcategories.interface.ts
@@ -3,9 +3,8 @@ export interface Subcategory {
   name: string; // Name of the category
   slug: string; // URL Slug
   description?: string; // Optional description of the category
-  products?: Product[]; // Array of products within this category
-  featured?: Product[]; // Array of featured products within this category
-  // subcategories: Subcategory[]; // Array of subcategories within this category
+  products?: string[]; // Array of products IDs within this category
+  featured?: string[]; // Array of featured IDs products within this category
   created: Date; // Date when the category was created
   updated: Date; // Date when the category was last updated
 }
