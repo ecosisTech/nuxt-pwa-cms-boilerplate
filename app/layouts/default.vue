aced583159e05130fc7f8d6a9814e853ea7e25f6
equaliser0 <accounts@equaliser.minimalcity.de>
2023-11-01 10:52:04 +0100

    facelift

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -168,7 +168,7 @@ onBeforeMount(async () => {
     <div class="" v-else>
       <!-- Navbar -->
       <div class="flex justify-center w-full my-4 fixed z-40">
-        <div class="navbar backdrop-blur-md bg-base-100/70 z-50 md:w-1/3 rounded rounded-full shadow mx-4">
+        <div class="navbar bg-base-100 z-50 md:w-1/3 rounded rounded-md shadow mx-4">
 
           <!-- Navbar Start -->
           <div class="navbar-start">
@@ -187,10 +187,10 @@ onBeforeMount(async () => {
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
               </label>
 
-              <ul class="dropdown-content bg-base-100 menu xl:menu-horizontal lg:min-w-max rounded-box mt-16">
-                <li v-for="category in categoriesStore.categories">
+              <ul class="dropdown-content relative flex flex-wrap bg-base-100 menu xl:menu-horizontal  w-[720px] rounded mt-16">
+                <li class="bg-base-100 m-1 rounded rounded-md flex-64" v-for="category in categoriesStore.categories">
                   <NuxtLink :to="`/shop/${category.slug}`">
-                    <img class="w-12 h-12" :src="'/uploads/' + category.image"/>
+                    <img class="w-12 h-12 rounded rouned-md" :src="'/uploads/' + category.image"/>
                     {{ category.name }}
                   </NuxtLink>
                   <ul v-for="subcategory in category.subcategories">
