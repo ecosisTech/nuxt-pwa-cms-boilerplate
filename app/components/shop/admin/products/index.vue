bab6a859a324f1fb6d9aaf29a8063ebf14554c1b
equaliser0 <accounts@equaliser.minimalcity.de>
2023-11-02 12:58:27 +0100

    fix table head

--- app/components/shop/admin/products/index.vue
+++ app/components/shop/admin/products/index.vue
@@ -107,7 +107,7 @@ const selected = ref([])
           <th>{{ item['brand'] }}</th>
           <th>{{ item['propertyName'] }}</th>
           <th>{{ item['propertyValue'] }}</th>
-          <!-- <th>{{ item['variants'][0] }}</th> -->
+          <th>{{ item['variants'][0] }}</th>
           <th>{{ item['properties'] }}</th>
           <th>{{ item['EAN'] }}</th>
           <th>{{ item['sellingPrice'] }}</th>
