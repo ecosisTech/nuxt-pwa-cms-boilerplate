75344cec8bef2f419bccf79d6b9e726405ea72ae
equaliser0 <accounts@equaliser.minimalcity.de>
2023-11-07 14:22:46 +0100

    facelift

--- app/components/shop/admin/orders/index.vue
+++ app/components/shop/admin/orders/index.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts">
+import { useOrdersStore } from '../../../../stores/orders'
+import { useNotificationStore } from '../../../../stores/notifications'
+
+
+const orderStore = useOrdersStore()
+const notificationStore = useNotificationStore()
+
 const router = useRouter()
 
-const orders = await getAllOrders()
+const orders = orderStore.orders
 
 const selected = ref([])
 
@@ -15,8 +22,21 @@ const propertyToSortWith = ref('name')
 const ascending = ref(true)
 
 const filteredOrders = computed(() => {
-  return sortArrayByProperty(orders, propertyToSortWith.value, ascending.value)
+  return sortArrayByProperty(orderStore.orders, propertyToSortWith.value, ascending.value)
 })
+
+const deliveredOrder = async (order) => {
+  try {
+    order.shipped = true
+    await updateOrder(order)
+    await orderStore.fetchOrders()
+  } catch (error) {
+    notificationStore.addNotification({
+      type: 'error',
+      msg: error
+    })
+  }
+}
 
 const sortWith = (prop) => {
   ascending.value = !ascending.value
@@ -73,11 +93,14 @@ const sortWith = (prop) => {
               <svg v-else xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
             </th>
             <th>
-              <svg v-if="order.delivered" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check"><polyline points="20 6 9 17 4 12"/></svg>
+              <svg v-if="order.shipped" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check"><polyline points="20 6 9 17 4 12"/></svg>
               <svg v-else xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
             </th>
             <th class="flex flex-wrap justify-center items-center">
-              <button class="btn btn-circle ml-2" @click="viewOrder(order)" onclick="orderView.showModal()">
+              <button class="btn btn-circle ml-2" :class="`${(!order.shipped) ? 'btn-success' : 'btn-ghost'}`" @click="deliveredOrder(order)" onclick="orderview.showModal()">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-clipboard-check"><rect width="8" height="4" x="8" y="2" rx="1" ry="1"/><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"/><path d="m9 14 2 2 4-4"/></svg>
+              </button>
+              <button class="btn btn-circle ml-2" @click="viewOrder(order)" onclick="orderview.showModal()">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eye"><path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z"/><circle cx="12" cy="12" r="3"/></svg>
               </button>
             </th>
@@ -101,7 +124,7 @@ const sortWith = (prop) => {
         </tfoot>
       </table>
       <!-- You can open the modal using ID.showModal() method -->
-      <dialog id="orderView" class="modal" v-if="orderDetails">
+      <dialog id="orderview" class="modal" v-if="orderDetails">
         <div class="modal-box w-11/12 max-w-5xl">
           <div class="flex flex-wrap justify-between">
             <div class="bg-base-200 rounded-md p-4 m-2 w-full">
--- app/components/shop/admin/orders/new.vue
+++ app/components/shop/admin/orders/new.vue
@@ -1,45 +1,13 @@
 <script setup lang="ts">
-const orders = await getAllOrders()
+import { useOrdersStore } from '../../../../stores/orders'
 
-//
-// const slider = ref(null)
-//
-// onMounted(() => {
-//   console.log(slider);
-//
-//   let isDown = false
-//   let startX
-//   let scrollLeft
-//
-//   slider.value.addEventListener('mousedown', (e) => {
-//     isDown = true
-//     slider.value.classList.add('active')
-//     startX = e.pageX - slider.value.offsetLeft
-//     scrollLeft = slider.value.scrollLeft
-//   })
-//   slider.value.addEventListener('mouseleave', () => {
-//     isDown = false
-//     slider.value.classList.remove('active')
-//   })
-//   slider.value.addEventListener('mouseup', () => {
-//     isDown = false
-//     slider.value.classList.remove('active')
-//   })
-//   slider.value.addEventListener('mousemove', (e) => {
-//     if(!isDown) return
-//     e.preventDefault()
-//     const x = e.pageX - slider.offsetLeft
-//     const walk = (x - startX) * 3 //scroll-fast
-//     slider.scrollLeft = scrollLeft - walk
-//     console.log(walk)
-//   })
-// })
+const orderStore = useOrdersStore()
 </script>
 
 <template>
   <section class="mt-5">
     <p class="font-bold text-center">Neue Bestellungen</p>
-    <div class="w-full flex justify-center">      
+    <div class="w-full flex justify-center">
       <NuxtLink class="flex shrink-0 flex-wrap items-center justify-center" to="/admin/shop/orders">
         <divider class="btn btn-circle bg-base-300 flex items-center justify-center mt-2">
           <!-- <Icon name="heroicons:plus" class="h-5 w-5" /> -->
@@ -53,21 +21,15 @@ const orders = await getAllOrders()
     <div
       class="mt-5 flex w-full items-start gap-6 overflow-x-auto rounded-lg border bg-base-100 px-5 py-5 scrollbar-thin scrollbar-thumb-input scrollbar-thumb-rounded-md mb-8"
     >
-      <div class="w-full flex justify-center items-center" v-if="!orders.length">
+      <div class="w-full flex justify-center items-center" v-if="!orderStore.orders.length">
         <h2>Keine Bestellungen</h2>
       </div>
-      <template v-for="n in data" v-else>
+      <template v-for="order in orderStore.order">
         <NuxtLink class="flex shrink-0 flex-col items-center gap-2" to="/admin/shop/oders">
-          <img
-            class="h-10 w-10 rounded-full object-cover"
-            :src="n.picture.thumbnail"
-            :alt="n.name.first"
-          />
           <div class="text-center">
-            <p class="text-sm font-semibold">{{ n.name.first }} {{ n.name.last }}</p>
-            <p class="text-xs text-muted-foreground text-gray-500">seit ca. 10min</p>
-            <p class="text-xs text-muted-foreground">3 Produkte</p>
-            <p class="text-xs text-muted-foreground text-secondary">46,50 €</p>
+            <p class="text-sm font-semibold">{{ order.totalGross }} {{ order.totalNet }}</p>
+            <p class="text-xs text-muted-foreground text-gray-500">{{ order.created }}</p>
+            <p class="text-xs text-muted-foreground">{{ order.products.length }}</p>
           </div>
         </NuxtLink>
       </template>
